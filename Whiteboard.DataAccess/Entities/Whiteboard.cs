243dc82e2148178e7323301b8ce1020a088a89a8
Lex de Kort <I488027@fontysict.nl>
2024-05-04 14:00:45 +0200

    Initialize repositories and DI

---
+++ Whiteboard.DataAccess/Entities/Whiteboard.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Whiteboard.DataAccess.Entities
+{
+    public class Whiteboard
+    {
+        [Key]
+        public int Id { get; set; }
+        [Key]
+        public Guid BoardId { get; set; }
+        public int OwnerId { get; set; }
+        public required string Name { get; set; }
+        public ICollection<int> MemberIds { get; set; } = [];
+
+        public Whiteboard() 
+        {
+            if(!MemberIds.Contains(OwnerId)) MemberIds.Add(OwnerId);
+        }
+    }
+}
--- Whiteboard.Service/Program.cs
+++ Whiteboard.Service/Program.cs
@@ -1,12 +1,14 @@
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Whiteboard.DataAccess.Repositories;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWebApplication()
     .ConfigureServices(services =>
     {
         services.AddApplicationInsightsTelemetryWorkerService();
+        services.AddScoped<IWhiteboardRepository, WhiteboardRepository>();
         services.ConfigureFunctionsApplicationInsights();
     })
     .Build();
